0552f4956791eebc561269de85eb04f78ade62c4
Victor Lin <13424970+victorlin@users.noreply.github.com>
2022-02-20 17:44:30 -0800

    update with viet

--- src/constants/config.ts
+++ src/constants/config.ts
@@ -1,11 +1,11 @@
 export const CONFIG = {
   tries: 6, // This changes how many tries you get to finish the wordle
-  language: 'YourLanguageHere', // This changes the display name for your language
+  language: 'Vietnamese', // This changes the display name for your language
   wordLength: 5, // This sets how long each word is based on how many characters (as defined in orthography.ts) are in each word
-  author: 'YourNameHere', // Put your name here so people know who made this Wordle!
-  authorWebsite: 'YourLinkHere', // Put a link to your website or social media here
-  wordListSource: 'YourDictionarySource', // Describe the source material for your words here
-  wordListSourceLink: 'YourDictionaryLink', // Put a link to the source material for your words here
+  author: 'victorlin', // Put your name here so people know who made this Wordle!
+  authorWebsite: 'victorl.in', // Put a link to your website or social media here
+  wordListSource: 'duyet/vietnamese-wordlist', // Describe the source material for your words here
+  wordListSourceLink: 'https://github.com/duyet/vietnamese-wordlist', // Put a link to the source material for your words here
   //
   // THESE NEXT SETTINGS ARE FOR ADVANCED USERS
   //
