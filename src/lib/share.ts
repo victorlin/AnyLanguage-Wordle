e9228f189550550cf4c1def8c6e3b53d0d7807d1
Victor Lin <13424970+victorlin@users.noreply.github.com>
2022-02-20 20:22:53 -0800

    styling

--- src/lib/share.ts
+++ src/lib/share.ts
@@ -5,6 +5,7 @@ import { CONFIG } from '../constants/config'
 export const shareStatus = (guesses: string[][], lost: boolean) => {
   navigator.clipboard.writeText(
     CONFIG.language +
+      ' Wordle ' +
       solutionIndex +
       ' ' +
       `${lost ? 'X' : guesses.length}` +
